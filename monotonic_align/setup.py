85800211a991cd6efbde89ff528ea0da7e618a03
Duong <tieuduong.25.1.98@gmail.com>
2025-03-30 01:30:23 +0700

    30/02_2

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
 setup(
     name="monotonic_align",
-    ext_modules=cythonize("core.pyx", language_level=3),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize("core.pyx"),
+    include_dirs=[numpy.get_include()],
 )
